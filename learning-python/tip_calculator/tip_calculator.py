c0aeab19f230572f98d4e33677f72523615f2e4d
Matt Barlow <matt.barlow@rackspace.com>
2013-10-28 10:55:32 -0500

    Used OptionParser to add options

--- learning-python/tip_calculator/tip_calculator.py
+++ learning-python/tip_calculator/tip_calculator.py
@@ -1,17 +1,26 @@
 #! /usr/bin/env python
-import sys
+from optparse import OptionParser
 
-meal = float(sys.argv[1])
-tax = float(sys.argv[2])
-tip = float(sys.argv[3])
+parser = OptionParser()
 
-tax_value = meal * (tax/100)
-meal_with_tax = meal + tax_value
-tip_value = meal_with_tax * (tip/100)
+parser.add_option("-m", "--meal", dest="meal", help="the amount of your meal",
+	type="float")
+parser.add_option("-t", "--tax", dest="tax", help="the tax percentage",
+	default="8.25",
+	type="float")
+parser.add_option("-i", "--tip", dest="tip", help="the tip percentage",
+	default="20",
+	type="float")
+
+(options, args) = parser.parse_args()
+
+tax_value = options.meal * (options.tax/100)
+meal_with_tax = options.meal + tax_value
+tip_value = meal_with_tax * (options.tip/100)
 
 total = meal_with_tax + tip_value
 
-print "The base cost of your meal is {:.2f}".format(meal)
+print "The base cost of your meal is {:.2f}".format(options.meal)
 print "The amount of tax added is {:.2f}".format(tax_value)
 print "The tip you will pay is {:.2f}".format(tip_value)
 print "The total amount you will pay is {:.2f}".format(total)
